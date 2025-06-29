8473ae8aa0f7eb4fd9e59a4321bf0f781d25eb30
bolillochan <ivonne.mendezw.gmail.com>
2025-06-29 01:31:33 -0600

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './component/home/home.component';
+import { AnimalesListComponent } from './component/animales-list/animales-list.component';
+import { AnimalFormComponent } from './component/animal-form/animal-form.component';
+import { AnimalDetailComponent } from './component/animal-detail/animal-detail.component';
+import { EstadisticasComponent } from './component/estadisticas/estadisticas.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'animales', component: AnimalesListComponent },
+  { path: 'animales/crear', component: AnimalFormComponent },
+  { path: 'animales/editar/:id', component: AnimalFormComponent },
+  { path: 'detalle/:id', component: AnimalDetailComponent },
+  { path: 'estadisticas', component: EstadisticasComponent },
+  { path: '**', redirectTo: '' }
+];
