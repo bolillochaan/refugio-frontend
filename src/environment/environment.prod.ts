458ed46cd96a35029d50ff533307c29b97260d99
bolillochan <ivonne.mendezw.gmail.com>
2025-06-30 22:32:57 -0600

    ya

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://tu-backend-url.com/api', // Cambia esto por la URL de tu backend
+  // Agrega aquí otras variables de entorno para producción
+}; 
